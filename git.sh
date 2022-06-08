a0193bb6865577239689c171408eb645928c9502
tommy.tang <tommy.tang@lixinchuxing.com>
2022-06-08 18:22:14 +0800

    opti: 细节优化

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "opti: 细节优化"
-# git push
-git push origin `git subtree split --prefix www developing`:gh-pages --force
+git push
+# git push origin `git subtree split --prefix www developing`:gh-pages --force
